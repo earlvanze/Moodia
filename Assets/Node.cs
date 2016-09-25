adcf5e23a68c86481ca00e639ac7a918970b44a3
Lucas Rizzotto <contact@lucasrizzotto.com>
2016-09-24 22:54:49 -0400

    Spawn Update
    Spawning multiple nodes, all that follow their own little orbits

---
+++ Assets/Node.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+    private int id;
+        
+    public Transform target;
+    public float RotationSpeed = 100f;
+    public float OrbitDegrees = 1f;
+
+    void Start()
+    {
+        gameObject.transform.rotation = Random.rotation;
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+        transform.RotateAround(target.position, Vector3.up, OrbitDegrees); 
+    }
+}
